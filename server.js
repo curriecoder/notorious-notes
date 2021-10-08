9ebebd53fcd7b68dca520be8925173b95a2d6560
curriecoder <andrew.j.currie0@gmail.com>
2021-10-07 20:20:52 -0400

    well that was a bad idea, back to the drawing board

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 // requiring modules
 const express = require('express');
 
-require('./routes/routes');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+require('./routes/routes');
 
 // set data parsing
 app.use(express.urlencoded({ extended: true}));
@@ -16,3 +16,61 @@ app.use(express.static('public'));
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
+
+// requiring modules
+const express = require("express");
+
+const path = require("path");
+const fs = require("fs");
+const app = express();
+
+
+// use index.html for static display 
+// app.use(express.static("public"));
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+// retrieve existing notes from db/db.json
+fs.readFile("db/db.json", "utf8", (err, data) => {
+  if (err) throw err;
+  
+  let notes = JSON.parse(data);
+  
+  function updateDb() {
+    fs.writeFile("db/db.json", JSON.stringify(notes, '/t'), (err, data) => {
+      if (err) throw err;
+      console.log(data);
+    });
+  }
+  
+  app.get("/api/notes", (req, res) => {
+    res.json(notes);
+  });
+  
+  // write notes to db/db.json
+  app.post("/api/notes", (req, res) => {
+    let newNote = req.body;
+    notes.push(newNote);
+    updateDb();
+    return console.log(`Added note: ${newNote.title}!`);
+  });
+
+  app.get('/api/notes/:id', (req, res) => {
+    res.json(notes[req.params.id]);
+  });
+
+  app.delete('/api/notes/id:', (req, res) => {
+    notes.splice(req.params.id, 1);
+    updateDb();
+    console.log(`Note ${req.params.id} deleted!`);
+  });
+});
+
+
+module.exports = app;
